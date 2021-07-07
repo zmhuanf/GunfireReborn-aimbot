d523e4affc9b1935e57f16c4369f6fdb02ba3170
zmhuanf <zmhuanf@163.com>
2021-07-07 22:03:23 +0800

    去除部分功能，适配最新游戏

--- Main.cs
+++ Main.cs
@@ -1,305 +1,126 @@
-﻿using MelonLoader;
-using UnityEngine;
-using UnityEngine.UI;
-using UnhollowerRuntimeLib;
+﻿using HeroCameraName;
 using Il2CppSystem.Collections.Generic;
+using MelonLoader;
 using System;
-using System.IO;
-using HeroCameraName;
-using Item;
-using BulletChange;
-using OCServerMoveNS;
-using DataHelper;
-using TMPro;
+using System.Diagnostics;
+using UnityEngine;
 
 namespace zcMod
 {
     public static class BuildInfo
     {
-        public const string Name = "zcMod"; // Name of the Mod.  (MUST BE SET)
-        public const string Description = "Gunfire Reborn Aimbot"; // Description for the Mod.  (Set as null if none)
-        public const string Author = "zhuchong"; // Author of the Mod.  (Set as null if none)
-        public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
-        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+        public const string Name = "zcMod";
+        public const string Description = "Gunfire Reborn Aimbot";
+        public const string Author = "Author:zhuchong; Modify:zmhuanf";
+        public const string Company = "tms";
+        public const string Version = "1.2.6";
+        public const string DownloadLink = null;
     }
 
     public class zcMod : MelonMod
     {
-        public static bool showobjinfo = false;
-        public static bool showallobjinfo = false;
-        public static bool limitangle = true;
-        public static bool shownpc = false;
-        public override void OnApplicationStart() // Runs after Game Initialization.
-        {
-            MelonLogger.Log("OnApplicationStart");
-        }
+        public static bool flag = true;
+        public static bool repeat_lock = true;
+        public static Stopwatch sw = new Stopwatch();
 
-        public override void OnLevelIsLoading() // Runs when a Scene is Loading or when a Loading Screen is Shown. Currently only runs if the Mod is used in BONEWORKS.
+        public override void OnUpdate()
         {
-            MelonLogger.Log("OnLevelIsLoading");
-        }
-
-        public override void OnLevelWasLoaded(int level) // Runs when a Scene has Loaded.
-        {
-            MelonLogger.Log("OnLevelWasLoaded: " + level.ToString());
-        }
-
-        public override void OnLevelWasInitialized(int level) // Runs when a Scene has Initialized.
-        {
-            MelonLogger.Log("OnLevelWasInitialized: " + level.ToString());
-        }
-
-        public override void OnUpdate() // Runs once per frame.
-        {
-            try
+            // 左Ctrl切换是否启用自动瞄准
+            if (Input.GetKey(KeyCode.LeftControl) && repeat_lock)
             {
-                if (HeroCameraManager.HeroObj != null && HeroCameraManager.HeroObj.BulletPreFormCom != null && HeroCameraManager.HeroObj.BulletPreFormCom.weapondict != null) 
-                {
-                    foreach (KeyValuePair<int, WeaponPerformanceObj> Weapon in HeroCameraManager.HeroObj.BulletPreFormCom.weapondict)
-                    {
-                        if (!limitangle)
-                        {
-                            Weapon.value.ModifyBulletInMagzine(100, 100);//不用换弹
-                            if (Weapon.value.WeaponAttr.Radius != 500f)
-                                Weapon.value.WeaponAttr.Radius = 500f;//爆炸范围
-                        }
-
-                        if (Weapon.value.WeaponAttr.Accuracy != 10000) 
-                            Weapon.value.WeaponAttr.Accuracy = 10000;//子弹不扩散
-                        if (Weapon.value.WeaponAttr.AttDis != 500f)
-                            Weapon.value.WeaponAttr.AttDis = 500f;//射程
-                        //Weapon.value.WeaponAttr.Pierce = 99; //穿透力，改了好像无效果?
-
-                        if (Weapon.value.WeaponAttr.BulletSpeed >= 50f && Weapon.value.WeaponAttr.BulletSpeed != 55f)//弹道速度,某些武器改了会打不到怪
-                        {
-                            if (Weapon.value.WeaponAttr.BulletSpeed != 500f)
-                                Weapon.value.WeaponAttr.BulletSpeed = 500f;
-                        }
-                        else if (Weapon.value.WeaponAttr.BulletSpeed == 30f) 
-                        {
-                            if (Weapon.value.WeaponAttr.BulletSpeed != 500f)
-                                Weapon.value.WeaponAttr.BulletSpeed = 500f;
-                        }
-                        if (Weapon.value.WeaponAttr.Stability != 10000)
-                            Weapon.value.WeaponAttr.Stability = 10000;//后坐力
-                        if (Weapon.value.WeaponAttr.Radius > 0f)
-                        {
-                            if (Weapon.value.WeaponAttr.Radius < 9f)
-                                Weapon.value.WeaponAttr.Radius = 9f;//爆炸范围(会影响爆炸类武器、火标和电手套)
-                        }
-                    }
-                }
-                if (Input.GetKeyUp(KeyCode.Home))//暴力模式全图锁
-                {
-                    shownpc = !shownpc;
-                }
-                if (Input.GetKeyUp(KeyCode.Delete))
-                {
-                    //if (WarShopManager.ShopItemDict.count > 0) 
-                    //{
-                    //    bool first = true;
-                    //    WarShopObject fobj = null;
-                    //    foreach (KeyValuePair<int, WarShopObject> shopitem in WarShopManager.ShopItemDict)
-                    //    {
-                    //        if (shopitem.value.PropertyDict.ContainsKey("Inscription"))
-                    //        {
-                    //            List<int> Inscription = new List<int>();
-                    //            Inscription.Add(4820); Inscription.Add(4821); Inscription.Add(4822); Inscription.Add(4823); Inscription.Add(4824); Inscription.Add(4825); Inscription.Add(4827);
-                    //            shopitem.value.PropertyDict["Inscription"] = Inscription;
-                    //        }
-                    //
-                    //    }
-                    //}
-                }
-                //War_Shop_Event
-                //InitWarShop
-
-
-                if (Input.GetKeyUp(KeyCode.Insert))//暴力模式全图锁
-                {
-                    limitangle = !limitangle;
-                }
-                
-                //if (Input.GetMouseButton(1)) 
-                if (Input.GetKey(KeyCode.F))//按F键自瞄(请按个人喜好修改)
+                flag = !flag;
+                repeat_lock = false;
+                sw.Restart();
+            }
+            if (sw.ElapsedMilliseconds >= 1000)
+            {
+                repeat_lock = true;
+                sw.Stop();
+            }
+            // 获取所有怪物
+            if (flag)
+            {
+                try
                 {
                     List<NewPlayerObject> monsters = NewPlayerManager.GetMonsters();
                     if (monsters != null)
                     {
-                        
                         Vector3 campos = CameraManager.MainCamera.position;
-
                         Transform nearmons = null;
                         float neardis = 99999f;
                         foreach (NewPlayerObject monster in monsters)
                         {
-                            Transform montrans = monster.BodyPartCom.GetWeakTrans();
+                            Transform montrans = null;
+                            try
+                            {
+                                montrans = monster.BodyPartCom.GetCenterTrans().parent.FindChild("Bip001 Neck").FindChild("Bip001 Head");
+                            }
+                            catch (Exception)
+                            {
+                                // 如无法获取怪物头部坐标则忽略该怪物
+                            }
                             if (montrans == null)
                             {
                                 continue;
                             }
-                            Vector3 vec = CameraManager.MainCameraCom.WorldToViewportPoint(montrans.position);
-                            if (limitangle)
-                            {
-                                if (vec.z <= 0) continue;
-                                vec.y = 0;
-                                vec.x = 0.5f - vec.x;
-                                vec.x = Screen.width * vec.x;
-                                vec.z = 0f;
-                                if (vec.magnitude > 150f) continue;//自瞄范围
-                            }
-                            vec = montrans.position - campos;
+                            // 获取距离
+                            Vector3 vec = montrans.position - campos;
                             float curdis = vec.magnitude;
+                            // 射线，判断是否可见
                             Ray ray = new Ray(campos, vec);
                             var hits = Physics.RaycastAll(ray, curdis);
                             bool visible = true;
-                            foreach(var hit in hits)
+                            foreach (var hit in hits)
                             {
-                                if (showallobjinfo)
-                                {
-                                    MelonLogger.Log(hit.collider.name);
-                                    MelonLogger.Log(hit.collider.gameObject.layer.ToString());
-                                }
-                                
                                 if (hit.collider.gameObject.layer == 0 || hit.collider.gameObject.layer == 30 || hit.collider.gameObject.layer == 31) //&& hit.collider.name.Contains("_")
                                 {
-                                    if (showobjinfo)
-                                    {
-                                        MelonLogger.Log(hit.collider.name);
-                                        MelonLogger.Log(hit.collider.gameObject.layer.ToString());
-                                    }
-
-                                    visible = false;//判断怪物是否可见
+                                    visible = false;
                                     break;
                                 }
                             }
+                            // 获取最近怪物
                             if (visible)
                             {
-                                
                                 if (curdis < neardis)
                                 {
                                     neardis = curdis;
                                     nearmons = montrans;
                                 }
                             }
-
-
                         }
+                        // 调整摄像机
                         if (nearmons != null)
                         {
-                            Vector3 objpos = new Vector3();
-                            objpos.x = HeroCameraManager.HeroObj.gameTrans.position.x;
-                            objpos.y = HeroCameraManager.HeroObj.gameTrans.position.y + 1f;
-                            objpos.z = HeroCameraManager.HeroObj.gameTrans.position.z;
+                            Vector3 objpos = HeroCameraManager.HeroObj.gameTrans.position;
                             Vector3 fw = nearmons.position - objpos;
-                            fw.y += 0.12f;
+                            fw.y = -0.1f;
                             Quaternion rot = Quaternion.LookRotation(fw);
                             HeroCameraManager.HeroObj.gameTrans.rotation = rot;
                             fw = nearmons.position - campos;
-                            fw.y += 0.12f;
+                            fw.y += 0.2f;
                             Quaternion rot2 = Quaternion.LookRotation(fw);
                             CameraManager.MainCamera.rotation = rot2;
-
                         }
                     }
                 }
-
-            }
-            catch
-            {
-
-            }
-        }
-
-        public override void OnFixedUpdate() // Can run multiple times per frame. Mostly used for Physics.
-        {
-        }
-
-        public override void OnLateUpdate() // Runs once per frame after OnUpdate and OnFixedUpdate have finished.
-        {
-        }
-
-        public override void OnGUI() // Can run multiple times per frame. Mostly used for Unity's IMGUI.
-        {
-            try
-            {
-                if (shownpc)
+                catch (Exception)
                 {
-                    foreach (var obj in NewPlayerManager.PlayerDict)
-                    {
-                        var val = obj.Value;
-                        if (val.centerPointTrans == null) continue;
-                        if (!ShowObject(val)) continue;
-                        var screenPos = CameraManager.MainCameraCom.WorldToScreenPoint(val.centerPointTrans.transform.position);
-                        if (screenPos.z > 0)
-                        {
-                            var dist = Vector3.Distance(HeroMoveManager.HeroObj.centerPointTrans.position, val.centerPointTrans.position).ToString("0.0");
-                            GUI.Label(new Rect(screenPos.x, Screen.height - screenPos.y, 800, 50), FightTypeToString(val) + "(" + dist + "m)");
-                        }
-                    }
+                    // 捕获所有错误
                 }
-                return;
             }
-            catch
+        }
+
+        // 测试用函数
+        public void Test(Transform t, int deep)
+        {
+            MelonLogger.Log("deep_" + deep + "_name:" + t.name);
+            MelonLogger.Log("deep_" + deep + "_pos:" + t.position.ToString());
+            deep += 1;
+            for (int i = 0; i < t.GetChildCount(); ++i)
             {
-                MelonLogger.Log("OnGUIbug");
+                Transform t2 = t.GetChild(i);
+                Test(t2, deep);
             }
         }
-
-        public override void OnApplicationQuit() // Runs when the Game is told to Close.
-        {
-            MelonLogger.Log("OnApplicationQuit");
-        }
-
-        public override void OnModSettingsApplied() // Runs when Mod Preferences get saved to UserData/modprefs.ini.
-        {
-            MelonLogger.Log("OnModSettingsApplied");
-        }
-
-        public override void VRChat_OnUiManagerInit() // Runs upon VRChat's UiManager Initialization. Only runs if the Mod is used in VRChat.
-        {
-            MelonLogger.Log("VRChat_OnUiManagerInit");
-        }
-
-        public bool ShowObject(NewPlayerObject obj)
-        {
-            if (obj.FightType == ServerDefine.FightType.NWARRIOR_DROP_EQUIP)
-                return true;
-            else if (obj.FightType == ServerDefine.FightType.NWARRIOR_DROP_RELIC)
-                return true;
-            else if (obj.FightType == ServerDefine.FightType.NWARRIOR_NPC_SMITH)
-                return true;
-            else if (obj.FightType == ServerDefine.FightType.NWARRIOR_NPC_SHOP)
-                return true;
-            else if (obj.FightType == ServerDefine.FightType.WARRIOR_OBSTACLE_NORMAL && (obj.Shape == 4406 || obj.Shape == 4419 || obj.Shape == 4427))
-                return true;
-            else if (obj.FightType == ServerDefine.FightType.NWARRIOR_NPC_EVENT)
-                return true;
-            else if (obj.FightType == ServerDefine.FightType.NWARRIOR_NPC_ITEMBOX)
-                return true;
-            return false;
-
-        }
-        public String FightTypeToString(NewPlayerObject obj)
-        {
-            if (obj.FightType == ServerDefine.FightType.NWARRIOR_DROP_EQUIP)
-                return DataMgr.GetWeaponData(obj.Shape).Name + " +" + obj.DropOPCom.WeaponInfo.SIProp.Grade.ToString();
-            else if (obj.FightType == ServerDefine.FightType.NWARRIOR_DROP_RELIC)
-                return DataMgr.GetRelicData(obj.DropOPCom.RelicSid).Name;
-            else if (obj.FightType == ServerDefine.FightType.NWARRIOR_NPC_SMITH)
-                return "工匠";
-            else if (obj.FightType == ServerDefine.FightType.NWARRIOR_NPC_SHOP)
-                return "商人";
-            else if (obj.FightType == ServerDefine.FightType.WARRIOR_OBSTACLE_NORMAL && (obj.Shape == 4406 || obj.Shape == 4419 || obj.Shape == 4427))
-                return "秘境";
-            else if (obj.FightType == ServerDefine.FightType.NWARRIOR_NPC_EVENT)
-                return "宝箱";
-            else if (obj.FightType == ServerDefine.FightType.NWARRIOR_NPC_ITEMBOX)
-                return "宝箱";
-            return "unk";
-        }
-
-
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# GunfireReborn-aimbot
-	基于 https://github.com/HerpDerpinstine/MelonLoader  
-	枪火重生的一个mod  
-	演示视频 https://www.bilibili.com/video/BV1Sp4y1Y7ge  
-	9月1号更新了npc秘境的透视(home键开关)  
-	透视代码来自于https://github.com/shalzuth/AutoGunfireReborn/blob/master/GunfireRebornMods/Mods/ExtraSensoryPerception.cs  
+### 简介
+
+GunfireReborn-aimbot是基于MelonLoader制作的枪火重生自瞄弱点mod。
+
+该仓库Fork自https://github.com/pentium1131/GunfireReborn-aimbot，适配了最新版游戏，去除了除自瞄外的所有功能。
+按左Ctrl可切换自瞄开关。
+
+本mod基于MelonLoader 2.x 版本，对于 3.x 版本不保证兼容性。
+
+&#x203C;请勿将本mod用于多人模式&#x203C;
+
+### 免责声明
+
+使用本mod所造成的一切后果，包括但不限于数据丢失、账号被封禁等，原作者和修改者均不承担任何责任。
